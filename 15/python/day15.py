d766f5e10e60fcec93e1b774c7209019bc630985
jramaswami <jramaswami@gmail.com>
2018-12-15 18:53:08 -0500

    Day 15: the struggle is real.

--- 15/python/day15.py
+++ 15/python/day15.py
@@ -2,202 +2,177 @@
 Advent of Code 2018
 Day 15: Beverage Bandits
 """
-
-class Posn:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __repr__(self):
-        return "Posn({}, {})".format(self.x, self.y)
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __lt__(self, other):
-        if self.y == other.y:
-            return self.x < other.x
-        return self.y < other.y
-
-    def __add__(self, other):
-        return Posn(self.x + other.x, self.y + other.y)
-
-    def manhattan_dist(self, other):
-        return abs(self.x - other.x) + abs(self.y - other.y)
-
-class Unit:
-    def __init__(self, posn, utype, uid, grid):
-        self.hitpoints = 200
-        self.attack_damage = 3
-        self.posn = posn
-        self.utype = utype
-        self.uid = uid
-        self.grid = grid
-        if self.utype == 'E':
-            self.enemy_type = 'G'
-        else:
-            self.enemy_type = 'E'
-
-    def is_my_enemy(self, other):
-        "Return true if other is an enemy."
-        if isinstance(other, str):
-            return self.enemy_type == other
-        return other.utype == self.enemy_type
-
-    def find_nearest_enemy_space(self):
-        "Find the nearest enemy space."
-        print(self, 'looking for nearest enemy space ...')
-        enemy_spaces = []
-        for enemy in [u for u in self.grid.units if self.is_my_enemy(u)]:
-            for space in self.grid.neighbors(enemy.posn):
-                if self.grid[space] == '.':
-                    enemy_spaces.append((space.manhattan_dist(self.posn), space))
-        if enemy_spaces:
-            enemy_spaces.sort()
-            print(self, 'found enemy spaces', enemy_spaces)
-            return enemy_spaces[0][1]
-        return None
-
-    def find_next_step(self):
-        "Find next step."
-        for posn0 in self.grid.neighbors(self.posn):
-            if self.is_my_enemy(self.grid[posn0]):
-                return None
-
-        print(self, 'looking for next step ...' )
-        enemy_space = self.find_nearest_enemy_space()
-        if enemy_space:
-            steps = sorted((st.manhattan_dist(enemy_space), st)
-                           for st in self.grid.neighbors(self.posn))
-            print(self, 'my possible steps', steps)
-            return steps[0][1]
-
-    def move(self):
-        next_step = self.find_next_step()
-        if next_step:
-            print(self, 'moving to', next_step)
-            self.grid[self.posn] = '.'
-            self.posn = next_step
-            self.grid[self.posn] = self
-
-    def die(self):
-        self.grid[self.posn] = '.'
-        self.grid.units.remove(self)
-
-    def attack(self):
-        print(self, 'looking for targets ...')
-        enemies_in_range = []
-        for posn in self.grid.neighbors(self.posn):
-            nbr = self.grid[posn]
-            if isinstance(nbr, Unit) and self.is_my_enemy(nbr):
-                enemies_in_range.append(nbr)
-        if enemies_in_range:
-            enemies_in_range.sort()
-            target = enemies_in_range[0]
-            print(self, 'attacking target', target)
-            target.hitpoints -= self.attack_damage
-            if target.hitpoints <= 0:
-                target.die()
-
-    def __repr__(self):
-        return "{}{}hp{}@{}".format(self.utype, self.uid, self.hitpoints, self.posn)
-
-    def __lt__(self, other):
-        if self.hitpoints == other.hitpoints:
-            return self.posn < other.posn
-        return self.hitpoints < other.hitpoints
+from collections import namedtuple
 
 
-class Grid:
-    def __init__(self, input_lines):
-        self.time = 0
-        self.grid = []
-        self.units = []
-        unit_id = 0
-        for y, line in enumerate(input_lines):
-            line = line.strip()
-            grid_row = []
-            for x, char in enumerate(line):
-                if char == 'E' or char == 'G':
-                    self.units.append(Unit(Posn(x, y), char, unit_id, self))
-                    unit_id += 1
-                    grid_row.append(self.units[-1])
+Posn = namedtuple('Posn', ['x', 'y'])
+
+
+def neighborhood(cave, posn):
+    "Return neighbors."
+    offsets = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+    neighbors = []
+    for dy, dx in offsets:
+        x = posn.x + dx
+        y = posn.y + dy
+        if x >= 0 and x < len(cave[0]) \
+        and y >= 0 and y <= len(cave) \
+        and cave[y][x] != '#':
+            neighbors.append(Posn(x, y))
+    return neighbors
+
+
+def is_enemy(me, other):
+    "Is the other an enemy?"
+    if other.isalpha():
+        return me.isupper() != other.isupper()
+    return False
+
+def bfs(cave, start):
+    me = cave[start.y][start.x]
+    print('bfs', me, '@', start)
+    queue = [[start, n] for n in neighborhood(cave, start)]
+    paths_to_enemy = []
+    while queue and not paths_to_enemy:
+        new_queue = []
+        for path in queue:
+            posn = path[-1]
+            for neighbor in neighborhood(cave, posn):
+                if neighbor in path:
+                    continue
+                other = cave[neighbor.y][neighbor.x]
+                if is_enemy(me, other):
+                    path.append(neighbor)
+                    paths_to_enemy.append(path)
+                elif other == '.':
+                    path0 = list(path)
+                    path0.append(neighbor)
+                    new_queue.append(path0)
+        queue = new_queue
+    return paths_to_enemy
+
+
+def get_best_paths(paths):
+    best_paths = []
+    best_posn = Posn(10000000, 1000000)
+    for path in paths:
+        reachable_square = path[-2]
+        if reachable_square.y < best_posn.y:
+            best_paths = [path]
+            best_posn = reachable_square
+        elif reachable_square.y == best_posn.y:
+            if reachable_square.x == best_posn.x:
+                best_paths.append(path)
+            elif reachable_square.x < best_posn.x:
+                best_paths = [path]
+                best_posn = reachable_square
+    return best_paths
+
+def get_best_move(paths):
+    "Return nearest move."
+    best_mv = Posn(10000000, 1000000)
+    for path in paths:
+        mv = path[1]
+        if mv.y < best_mv.y:
+            best_mv = mv
+        elif mv.y == best_mv.y:
+            if mv.x < best_mv.x:
+                best_mv = mv
+    return best_mv
+
+
+def get_next_move(cave, unit_posn):
+    "Get the unit at given position's next move."
+    paths = bfs(cave, unit_posn)
+    if paths:
+        best_paths = get_best_paths(paths)
+        mv = get_best_move(best_paths)
+        return mv
+
+
+def parse_cave(input_lines):
+    "Parse map of cave."
+    cave = []
+    elf_id = 0
+    goblin_id = 0
+    ord_a = ord('a')
+    ord_A = ord('A')
+    for line in input_lines:
+        cave_row = []
+        line = line.strip()
+        for char in line:
+            if char == 'E':
+                elf = ord_A + elf_id
+                elf_id += 1
+                cave_row.append(chr(elf))
+            elif char == 'G':
+                goblin = ord_a + goblin_id
+                goblin_id += 1
+                cave_row.append(chr(goblin))
+            else:
+                cave_row.append(char)
+        cave.append(cave_row)
+    return cave
+
+
+def cave_to_string(cave, no_ids=False):
+    "Make cave a string."
+    cave0 = []
+    for row in cave:
+        row0 = []
+        for c in row:
+            if no_ids:
+                if c.isalpha():
+                    if c.islower():
+                        row0.append('G')
+                    else:
+                        row0.append('E')
                 else:
-                    grid_row.append(char)
-            self.grid.append(grid_row)
-        self.wd = len(self.grid[0])
-        self.ht = len(self.grid)
-
-    def inbounds(self, posn):
-        "Return True if x and y are in the grid."
-        return posn.x >= 0 and posn.x < self.wd \
-               and posn.y >= 0 and posn.y < self.ht
-
-    def neighbors(self, posn):
-        "Get four neighboring squares for x, y"
-        offsets = [Posn(0, -1), Posn(-1, 0), Posn(1, 0), Posn(0, 1)]
-        nbrs = []
-        for delta in offsets:
-            posn0 = posn + delta
-            if self.inbounds(posn0):
-                nbrs.append(posn0)
-        return nbrs
-
-    def tick(self):
-        "Tick."
-        self.time += 1
-        print('Tick', self.time)
-        for unit in self.units:
-            unit.move()
-            unit.attack()
-        print(self.units)
-        print('*****')
+                    row0.append(c)
+            else:
+                row0.append(c)
+        cave0.append(row0)
+    return "\n".join("".join(r) for r in cave0)
 
 
-    def __getitem__(self, key):
-        if isinstance(key, Posn):
-            return self.grid[key.y][key.x]
-        return self.grid[key]
+def move(cave, posn):
+    me = cave[posn.y][posn.x]
+    for neighbor in neighborhood(cave, posn):
+        if is_enemy(me, cave[neighbor.y][neighbor.x]):
+            print(me, 'engaged in combat @', posn)
+            return
 
-    def __setitem__(self, key, value):
-        if isinstance(key, Posn):
-            self.grid[key.y][key.x] = value
-
-    def __repr__(self):
-        repr_rows = []
-        for row in self.grid:
-            repr_row = []
-            for cell in row:
-                if isinstance(cell, Unit):
-                    repr_row.append(cell.utype)
-                else:
-                    repr_row.append(cell)
-            repr_rows.append("".join(repr_row))
-        return "\n".join(repr_rows)
+    print(me, 'moving')
+    next_move = get_next_move(cave, posn)
+    cave[posn.y][posn.x] = '.'
+    cave[next_move.y][next_move.x] = me
 
 
-def grid_from_file(file_name):
-    "Get grid from a file."
-    with open(file_name) as input_file:
-        return Grid(input_file.readlines())
+def tick(cave):
+    moved = set()
+    for y in range(len(cave)):
+        for x in range(len(cave[0])):
+            if cave[y][x].isalpha():
+                print(moved)
+                if cave[y][x] not in moved:
+                    moved.add(cave[y][x])
+                    move(cave, Posn(x, y))
+    print(cave_to_string(cave))
 
 
-
-
-def solve_a():
-    "Solve first part of puzzle."
-    pass
-
-
-def solve_b():
-    "Solve second part of puzzle."
-    pass
+def cave_from_file(filename):
+    "Get cave from a file."
+    with open(filename) as inputf:
+        cave = parse_cave(inputf.readlines())
+        return cave
 
 
 def main():
     "Main program."
-    pass
+    import sys
+    cave = parse_cave(sys.stdin.readlines())
+    print(cave_to_string(cave, no_ids=True))
+
 
 if __name__ == '__main__':
     main()
-
--- 15/python/test_day15.py
+++ 15/python/test_day15.py
@@ -1,58 +1,9 @@
-"""
-Advent of Code 2018
-Day 15: Beverage Bandits
-Tests
-"""
 import day15 as d15
 
-def test_find_nearest_enemy_space():
-    grid = d15.grid_from_file('../test_grid2.txt')
-    assert grid.units[0].find_nearest_enemy_space() == d15.Posn(3, 1)
 
-def test_find_next_step():
-    grid = d15.Grid(['#######', '#.E...#', '#.....#', '#...G.#', '#######'])
-    assert grid.units[0].find_nearest_enemy_space() == d15.Posn(4, 2)
-    assert grid.units[0].find_next_step() == d15.Posn(3, 1)
-
-def test_tick():
-    init = ['#########', '#G..G..G#', '#.......#', '#.......#', '#G..E..G#',
-            '#.......#', '#.......#', '#G..G..G#', '#########']
-    expected = [['#########', '#.G...G.#', '#...G...#', '#...E..G#', '#.G.....#',
-                 '#.......#', '#G..G..G#', '#.......#', '#########'],
-                ['#########', '#..G.G..#', '#...G...#', '#.G.E.G.#', '#.......#',
-                 '#G..G..G#', '#.......#', '#.......#', '#########'],
-                ['#########', '#.......#', '#..GGG..#', '#..GEG..#', '#G..G...#',
-                 '#......G#', '#.......#', '#.......#', '#########']]
-    grid = d15.Grid(init)
-    for t in range(3):
-        grid.tick()
-        assert str(grid) == "\n".join(expected[t])
-
-def test_combat():
-    init = ['#######', '#.G...#', '#...EG#', '#.#.#G#', '#..G#E#', '#.....#', '#######']
-    grid = d15.Grid(init)
-
-    # after round 1
-    grid.tick()
-    assert str(grid) == '#######\n#..G..#\n#...EG#\n#.#G#G#\n#...#E#\n#.....#\n#######'
-    assert len(grid.units) == 6
-    assert grid[d15.Posn(3, 1)].utype == 'G'
-    assert grid[d15.Posn(3, 1)].hitpoints == 200
-    assert grid[d15.Posn(4, 2)].utype == 'E'
-    assert grid[d15.Posn(4, 2)].hitpoints == 197
-
-    # after round 2
-    grid.tick()
-    assert str(grid) == '#######\n#...G.#\n#..GEG#\n#.#.#G#\n#...#E#\n#.....#\n#######'
-
-    # after round 23
-    while grid.time < 23:
-        grid.tick()
-    assert str(grid) == '#######\n#...G.#\n#..G.G#\n#.#.#G#\n#...#E#\n#.....#\n#######'
-    assert len(grid.units) == 5
-
-    # after round 24
-    grid.tick()
-    print(grid)
-    assert str(grid) == '#######\n#.G...#\n#..G..#\n#.#.#G#\n#..G#E#\n#.....#\n#######'
-
+def test_next_move():
+    cave = d15.parse_cave(['#######', '#E..G.#', '#...#.#', '#.G.#G#', '#######'])
+    assert d15.get_next_move(cave, d15.Posn(1, 1)) == d15.Posn(2, 1)
+    cave = d15.parse_cave(['#######', '#.E...#', '#.....#', '#...G.#', '#######'])
+    print(d15.cave_to_string(cave))
+    assert d15.get_next_move(cave, d15.Posn(2, 1)) == d15.Posn(3, 1)
