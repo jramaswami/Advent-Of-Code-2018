657deed37f287d8605ee363f58fc2edd34232a90
jramaswami <jramaswami@gmail.com>
2018-12-19 17:44:35 -0500

    Day 19: Intermediate commit.

---
+++ 19/python/program.py
@@ -0,0 +1,29 @@
+def main():
+    reg0 = 0
+    # reg1 = 867       # a
+    reg1 = 10551267  # b
+    reg2 = 0
+    reg3 = 1
+    # reg4 = 31 # a
+    reg4 = 10550400  # b
+    reg5 = 0
+    print(reg0, reg1, reg2, reg3, reg4, reg5)
+    while True:
+        reg2 = 1
+        while True:
+            reg4 = reg3 * reg2
+            if reg4 == reg1:
+                reg0 += reg3
+                print(reg0, reg1, reg2, reg3, reg4, reg5)
+            else:
+                reg2 += 1
+            reg2 += 1
+            if reg2 > reg1:
+                break
+        reg3 += 1
+        if reg3 > reg1:
+            break
+    print(reg0, reg1, reg2, reg3, reg4, reg5)
+
+if __name__ == '__main__':
+    main()
