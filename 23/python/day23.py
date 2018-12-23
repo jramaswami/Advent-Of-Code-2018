afd2941ab120e6cb6e49408af997fbfa40fefae7
jramaswami <jramaswami@gmail.com>
2018-12-23 14:00:12 -0500

    Day 23: Intermediate commit.

--- 23/python/day23.py
+++ 23/python/day23.py
@@ -41,28 +41,33 @@ def solve_a(nanobots):
 def solve_b(nanobots):
     events_x = []
     max_z_box = 0
-    max_z_boxes = set()
+    max_coords = None
     for n in nanobots:
         events_x.append((1, n.posn.x - n.radius, n))
         events_x.append((-1, n.posn.x + n.radius, n))
     nanobots_by_x = sorted(events_x, key=lambda a: a[1])
     box_x = set()
-    for t, x, n in nanobots_by_x:
-        if t == -1:
-            box_x.discard(n)
-        elif t == 1:
-            box_x.add(n)
+    for (tx, x, nx), (tx0, x0, nx0) in tqdm(list(zip(nanobots_by_x[:-1],nanobots_by_x[1:]))):
+        if tx == -1:
+            box_x.discard(nx)
+        elif tx == 1:
+            box_x.add(nx)
+        if len(box_x) < max_z_box:
+            continue
         events_y = []
         for n in box_x:
             events_y.append((1, n.posn.y - n.radius, n))
             events_y.append((-1, n.posn.y + n.radius, n))
         nanobots_by_y = sorted(events_y, key=lambda a: a[1])
         box_y = set()
-        for t, y, n in nanobots_by_y:
-            if t == -1:
-                box_y.discard(n)
-            elif t == 1:
-                box_y.add(n)
+        for (ty, y, ny), (ty0, y0, ny0) in zip(nanobots_by_y[:-1], nanobots_by_y[1:]):
+            if ty == -1:
+                box_y.discard(ny)
+            elif ty == 1:
+                box_y.add(ny)
+
+            if len(box_y) < max_z_box:
+                continue
 
             events_z = []
             for n in box_y:
@@ -70,31 +75,34 @@ def solve_b(nanobots):
                 events_z.append((-1, n.posn.z + n.radius, n))
             nanobots_by_z = sorted(events_z, key=lambda a: a[1])
             box_z = set()
-            for t, z, n in nanobots_by_z:
-                if t == -1:
-                    box_z.discard(n)
+            for (tz, z, nz), (tz0, z0, nz0) in zip(nanobots_by_z[:-1], nanobots_by_z[1:]):
+                if tz == -1:
+                    box_z.discard(nz)
                     continue
-                elif t == 1:
-                    box_z.add(n)
+                elif tz == 1:
+                    box_z.add(nz)
 
                 if len(box_z) > max_z_box:
                     max_z_box = len(box_z)
-                    max_z_boxes = set([frozenset(box_z)])
-                    # print('new', box_z, len(box_z), max_z_boxes)
-                elif max_z_box and len(box_z) == max_z_box:
-                    max_z_boxes.add(frozenset(box_z))
-                    # print('appending', box_z, len(box_z), max_z_boxes)
+                    max_coords = ((x, x0), (y, y0), (z, z0))
+                    if max_z_box == 971 or max_z_box == 5:
+                        print('max_z_box', max_z_box, 'x', x, 'x0', x0, 'y', y, 'y0', y0, 'z', z, 'z0', z0)
+                        print('x', nx0, nx0.posn.x + nx0.radius, nx0.posn.y + nx0.radius, nx0.posn.z + nx0.radius)
+                        print('y', ny0, ny0.posn.x + ny0.radius, ny0.posn.y + ny0.radius, ny0.posn.z + ny0.radius)
+                        print('z', nz0, nz0.posn.x + nz0.radius, nz0.posn.y + nz0.radius, nz0.posn.z + nz0.radius)
+                        print(box_z)
+
 
     print('***')
-    # for b in max_z_boxes:
-        # print(b)
     print(max_z_box)
+    (x0, x1), (y0, y1), (z0, z1) = max_coords
 
 
 def main():
     "Main program."
     import sys
     nanobots = [parse_nanobot(ln) for ln in sys.stdin]
+    print(len(nanobots), 'nanobots')
     # print(solve_a(nanobots))
     print(solve_b(nanobots))
 
